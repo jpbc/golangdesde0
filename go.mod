30d6179e9ba79980b20c1f91d74a53526c0c308b
jpbc <jpbc80@gmail.com>
2025-02-23 17:50:26 -0300

    Primer Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/curso/golandesde0
+
+go 1.24.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/curso/golandesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
